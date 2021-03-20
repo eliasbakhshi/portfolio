3f78e6b82988bcdc523380a2872bd1880c986dcd
Elias <bakhshielias@gmail.com>
2021-03-20 13:07:51 +0100

    Front-end is ready to use.

--- main.js
+++ main.js
@@ -30,17 +30,49 @@ form.addEventListener("submit", (e) => {
     });
 });
 
-
+/* It changes the tab color for the mobile users */
 document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#123456');
 
+/* Toggle the menu */
+let menuIcon = document.querySelector(".hamburger-icon");
+let hamburgerMenu = document.querySelector(".nav");
+menuIcon.addEventListener("click", icon => {
+    menuIcon.classList.toggle("opened");
+    hamburgerMenu.classList.toggle("opened");
+});
 
+/* Close the menu when user clicks or scrolls outside of that in tablet view. */
+function closeMenu(e) {
+    let openedNav = document.querySelector(".nav.opened");
+    let navIcon = document.querySelector(".hamburger-icon");
+    if (openedNav !== null) {
+        if ( !openedNav.contains(e.target) && !navIcon.contains(e.target) ){
+            navIcon.classList.toggle("opened");
+            openedNav.classList.toggle("opened");
+        }
+    }
+}
 
+window.addEventListener("click", closeMenu);
+window.addEventListener("scroll", closeMenu);
 
-
+/* Give the menu bg when it is leaving the home page. */
+let el = document.querySelector("#about");
+let tabletHeader = document.querySelector(".tablet .nav-bg");
+window.addEventListener("scroll", e => {
+    if (el.getBoundingClientRect().top < 120) {
+        // console.log(el.getBoundingClientRect().top)
+        tabletHeader.classList.add("active");
+    } else {
+        tabletHeader.classList.remove("active");
+    }
+});
 
 // Foo the smooth scrolling
-let scroll = new SmoothScroll('.nav a[href*="#"]', {
-    speed: 1000,
-    speedAsDuration: true,
-    easing: 'easeInCubic'
-});
+if (SmoothScroll != undefined) {
+    let scroll = new SmoothScroll('a.take-coffee[href*="#"], .nav a[href*="#"]', {
+        speed: 1000,
+        speedAsDuration: true,
+        easing: 'easeInCubic'
+    });
+}
